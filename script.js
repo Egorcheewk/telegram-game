9b590fe389d347617452ed0450e765221f654844
Egorcheewk <88950238+Egorcheewk@users.noreply.github.com>
2024-11-16 22:35:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ class Layer {
 const layers = [
     new Layer("assets/nightwalk bg mid.png", 1, 1.3, -100), // Передний слой
     new Layer("assets/nightwalk bg forest.png", 0.5, 1.1, 0), // Средний слой
-    new Layer("assets/nightwalk bg 1 low.png", 0.2, 1, 20) // Задний слой
+    new Layer("assets/nightwalk bg 1 low.png", 0.2, 1, -10) // Задний слой
 ];
 
 // Изменение логики отрисовки
