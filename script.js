821175f32b243a06f4728c9114aa68570b8f46e8
Egorcheewk <88950238+Egorcheewk@users.noreply.github.com>
2024-11-16 21:40:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -55,9 +55,10 @@ class Layer {
 // Создание слоёв
 const layers = [
     
-    new Layer("assets/nightwalk bg 1 low.png", 0.2, 1, 0), // Задний слой
+    
     new Layer("assets/nightwalk bg mid.png", 1, 1.3, -100) // Передний слой
     new Layer("assets/nightwalk bg forest.png", 0.5, 1.1, 0), // Средний слой
+    new Layer("assets/nightwalk bg 1 low.png", 0.2, 1, 0), // Задний слой
 ];
 
 // Главный игровой цикл
